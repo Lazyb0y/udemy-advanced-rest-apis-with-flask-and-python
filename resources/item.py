d54a6925e1a44a81691a497e4c997613774377bf
Foysal Ahamed <thisisfoysal@gmail.com>
2022-03-22 17:33:21 +0600

    Remove some jwt restrictions from item resource

--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt, get_jwt_identity
+from flask_jwt_extended import jwt_required
 from models.item import ItemModel
 
 
@@ -16,7 +16,6 @@ class Item(Resource):
                         help="Every item needs a store_id."
                         )
 
-    @jwt_required()
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
@@ -41,10 +40,6 @@ class Item(Resource):
 
     @jwt_required()
     def delete(self, name):
-        claims = get_jwt()
-        if not claims['is_admin']:
-            return {'message': 'Admin privilege required.'}, 401
-
         item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
@@ -68,13 +63,5 @@ class Item(Resource):
 
 class ItemList(Resource):
 
-    @jwt_required(optional=True)
     def get(self):
-        user_id = get_jwt_identity()
-        items = [item.json() for item in ItemModel.find_all()]
-        if user_id:
-            return {'items': items}
-        return {
-            'items': [item['name'] for item in items],
-            'message': 'More data available if you log in.'
-        }
+        return {'items': [item.json() for item in ItemModel.find_all()]}
