1c0d6adae4b8f0511552c45719c63592e7b3e71a
Foysal Ahamed <thisisfoysal@gmail.com>
2022-03-22 17:23:52 +0600

    Remove jwt events

--- app.py
+++ app.py
@@ -25,59 +25,12 @@ def create_tables():
 jwt = JWTManager(app)
 
 
-@jwt.additional_claims_loader
-def add_claims_to_jwt(identity):
-    if identity == 1:
-        return {'is_admin': True}
-    return {'is_admin': False}
-
-
 @jwt.token_in_blocklist_loader
 def check_if_token_in_blacklist(jwt_header, jwt_payload):
     jti = jwt_payload['jti']
     if jti in BLACKLIST:
         return True
     return False
-
-
-@jwt.expired_token_loader
-def expired_token_callback(jwt_header, jwt_payload):
-    return jsonify({
-        'description': 'The token has expired.',
-        'error': 'TOKEN_EXPIRED'
-    }), 401
-
-
-@jwt.invalid_token_loader
-def invalid_token_callback(error):
-    return jsonify({
-        'description': error,
-        'error': 'INVALID_TOKEN'
-    }), 401
-
-
-@jwt.needs_fresh_token_loader
-def token_not_fresh_callback(jwt_header, jwt_payload):
-    return jsonify({
-        'description': 'The token is not fresh.',
-        'error': 'FRESH_TOKEN_REQUIRED'
-    }), 401
-
-
-@jwt.revoked_token_loader
-def revoked_token_callback(jwt_header, jwt_payload):
-    return jsonify({
-        'description': 'The token has been revoked.',
-        'error': 'TOKEN_REVOKED'
-    }), 401
-
-
-@jwt.unauthorized_loader
-def missing_token_callback(error):
-    return jsonify({
-        'description': error,
-        'error': 'AUTHORIZATION_REQUIRED'
-    }), 401
 
 
 api.add_resource(Store, '/store/<string:name>')
