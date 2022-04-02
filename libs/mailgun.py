38dc941aa0690fd1d79ca218ce8f949c8f57b6a7
Foysal Ahamed <thisisfoysal@gmail.com>
2022-04-03 00:44:30 +0600

    Use .env to store mailgun keys

--- libs/mailgun.py
+++ libs/mailgun.py
@@ -1,10 +1,11 @@
+import os
 from typing import List
 from requests import Response, post
 
 
 class Mailgun:
-    MAILGUN_DOMAIN = "sandboxe8c60dc8add04d689d81a59b032e5cc4.mailgun.org"
-    MAILGUN_API_KEY = "bedc25ef79c15a3be0bbeccd8fa8d095-62916a6c-5d8ec552"
+    MAILGUN_DOMAIN = os.environ.get("MAILGUN_DOMAIN")
+    MAILGUN_API_KEY = os.environ.get("MAILGUN_API_KEY")
 
     FROM_TITLE = "Store REST API"
     FROM_EMAIL = "postmaster@sandboxe8c60dc8add04d689d81a59b032e5cc4.mailgun.org"
