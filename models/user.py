6b2e17cec399aae9a91501c4e888580338de93e3
Foysal Ahamed <thisisfoysal@gmail.com>
2022-04-02 23:00:56 +0600

    Send confirmation mail using mailgun

--- models/user.py
+++ models/user.py
@@ -1,4 +1,11 @@
 from db import db
+from flask import request, url_for
+from requests import Response, post
+
+MAILGUN_DOMAIN = "sandboxe8c60dc8add04d689d81a59b032e5cc4.mailgun.org"
+MAILGUN_API_KEY = "bedc25ef79c15a3be0bbeccd8fa8d095-62916a6c-5d8ec552"
+FROM_TITLE = "Store REST API"
+FROM_EMAIL = "postmaster@sandboxe8c60dc8add04d689d81a59b032e5cc4.mailgun.org"
 
 
 class UserModel(db.Model):
@@ -7,7 +14,12 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False, unique=True)
     activated = db.Column(db.Boolean, default=False)
+
+    @classmethod
+    def find_by_email(cls, email: str) -> "UserModel":
+        return cls.query.filter_by(email=email).first()
 
     @classmethod
     def find_by_username(cls, username: str) -> "UserModel":
@@ -20,6 +32,20 @@ class UserModel(db.Model):
     def save_to_db(self) -> None:
         db.session.add(self)
         db.session.commit()
+
+    def send_confirmation_email(self) -> Response:
+        link = request.url_root[:-1] + url_for("userconfirm", user_id=self.id)
+
+        return post(
+            f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages",
+            auth=("api", MAILGUN_API_KEY),
+            data={
+                "from": f"{FROM_TITLE} <{FROM_EMAIL}>",
+                "to": [self.email],
+                "subject": "Registration Confirmation",
+                "text": f"Please click the link to confirm your registration {link}",
+            },
+        )
 
     def delete_from_db(self) -> None:
         db.session.delete(self)
--- resources/user.py
+++ resources/user.py
@@ -1,3 +1,5 @@
+import traceback
+
 from flask import request, make_response, render_template
 from flask_jwt_extended import (
     create_access_token,
@@ -13,14 +15,17 @@ from blacklist import BLACKLIST
 from models.user import UserModel
 from schemas.user import UserSchema
 
+EMAIL_ALREADY_EXISTS = "A user with that email already exists."
+FAILED_TO_CREATE = "Internal server error. Failed to create user."
 USER_ALREADY_EXISTS = "A user with that username already exists."
-CREATED_SUCCESSFULLY = "User created successfully."
 NOT_CONFIRMED_ERROR = "You have not confirmed registration, please check your email <{}>."
 USER_NOT_FOUND = "User not found."
 USER_DELETED = "User deleted."
 INVALID_CREDENTIALS = "Invalid credentials!"
 USER_LOGGED_OUT = "User <id={}> successfully logged out."
 USER_CONFIRMED = "The user has been activated successfully."
+SUCCESS_REGISTER_MESSAGE = "Account created successfully, an email with an activation link has been send to your " \
+                           "email address. Please check your email for more details. "
 
 user_schema = UserSchema()
 
@@ -34,9 +39,16 @@ class UserRegister(Resource):
         if UserModel.find_by_username(user.username):
             return {"message": USER_ALREADY_EXISTS}, 400
 
-        user.save_to_db()
+        if UserModel.find_by_email(user.email):
+            return {"message": EMAIL_ALREADY_EXISTS}, 400
 
-        return {"message": CREATED_SUCCESSFULLY}, 201
+        try:
+            user.save_to_db()
+            user.send_confirmation_email()
+            return {"message": SUCCESS_REGISTER_MESSAGE}, 201
+        except:
+            traceback.print_exc()
+            return {"message": FAILED_TO_CREATE}, 500
 
 
 class User(Resource):
@@ -60,7 +72,7 @@ class UserLogin(Resource):
     @classmethod
     def post(cls):
         user_json = request.get_json()
-        user_data = user_schema.load(user_json)
+        user_data = user_schema.load(user_json, partial=("email",))
 
         user = UserModel.find_by_username(user_data.username)
         if user and safe_str_cmp(user.password, user_data.password):
