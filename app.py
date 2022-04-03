fedcd4e7ed548f06fd65dfbac753c0add20da32f
Foysal Ahamed <thisisfoysal@gmail.com>
2022-04-04 00:05:11 +0600

    Fix invalid resource uses error

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ from blacklist import BLACKLIST
 from db import db
 from ma import ma
 from resources.user import (
-    UserConfirm,
     UserRegister,
     User,
     UserLogin,
     UserLogout,
     TokenRefresh,
 )
+from resources.confirmation import Confirmation, ConfirmationByUser
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -54,7 +54,8 @@ api.add_resource(Store, "/store/<string:name>")
 api.add_resource(StoreList, "/stores")
 api.add_resource(Item, "/item/<string:name>")
 api.add_resource(ItemList, "/items")
-api.add_resource(UserConfirm, "/user_confirm/<int:user_id>")
+api.add_resource(Confirmation, "/user_confirmation/<string:confirmation_id>")
+api.add_resource(ConfirmationByUser, "/confirmation/user/<int:user_id>")
 api.add_resource(UserRegister, "/register")
 api.add_resource(User, "/user/<int:user_id>")
 api.add_resource(UserLogin, "/login")
