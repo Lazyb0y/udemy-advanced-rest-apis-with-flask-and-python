e07d6402d0fc58dca0694eef60065978d72a2b9e
Foysal Ahamed <thisisfoysal@gmail.com>
2022-04-03 21:46:54 +0600

    Add confirmation resource and update user model to use new resource

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@ from flask import request, url_for
 from requests import Response
 
 from libs.mailgun import Mailgun
+from models.confirmation import ConfirmationModel
 
 
 class UserModel(db.Model):
@@ -12,7 +13,15 @@ class UserModel(db.Model):
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(80), nullable=False, unique=True)
-    activated = db.Column(db.Boolean, default=False)
+
+    confirmation = db.relationship(
+        "ConfirmationModel", lazy="dynamic", cascade="all, delete-orphan"
+    )
+
+    @property
+    def most_recent_confirmation(self) -> "ConfirmationModel":
+        # ordered by expiration time (in descending order)
+        return self.confirmation.order_by(db.desc(ConfirmationModel.expire_at)).first()
 
     @classmethod
     def find_by_email(cls, email: str) -> "UserModel":
@@ -31,7 +40,7 @@ class UserModel(db.Model):
         db.session.commit()
 
     def send_confirmation_email(self) -> Response:
-        link = request.url_root[:-1] + url_for("userconfirm", user_id=self.id)
+        link = request.url_root[:-1] + url_for("confirmation", confirmation_id=self.most_recent_confirmation.id)
 
         subject = "Registration Confirmation"
         text = f"Please click the link to confirm your registration {link}"
