1d1587b7a63639fa565a200796edd6bb1d42f59f
Foysal Ahamed <thisisfoysal@gmail.com>
2022-04-11 13:08:47 +0600

    Handel if error occurred while github authentication

--- resources/github_login.py
+++ resources/github_login.py
@@ -1,4 +1,4 @@
-from flask import g
+from flask import g, request
 from flask_jwt_extended import create_access_token, create_refresh_token
 from flask_restful import Resource
 from schemas.user import UserSchema
@@ -21,6 +21,13 @@ class GithubAuthorize(Resource):
     @classmethod
     def get(cls):
         resp = github.authorized_response()
+        if resp is None or resp.get("access_token") is None:
+            error_response = {
+                "error": request.args["error"],
+                "error_description": request.args["error_description"],
+            }
+            return error_response
+
         g.access_token = resp["access_token"]
         github_user = github.get("user")
         github_username = github_user.data["login"]
